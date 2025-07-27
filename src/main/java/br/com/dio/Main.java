fde8540a8e1a36600a0f49de5ef063e456932fb4
Leonardo Castanheiro <leocastanheiro2006@gmail.com>
2025-07-27 19:26:57 -0300

    main update

--- src/main/java/br/com/dio/Main.java
+++ src/main/java/br/com/dio/Main.java
@@ -1,21 +1,16 @@
 package br.com.dio;
 
 import br.com.dio.persistence.migration.MigrationStrategy;
+import br.com.dio.ui.MainMenu;
 
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import static br.com.dio.persistence.config.ConnectionConfig.getConnection;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        MainMenu mainMenu = new MainMenu();
 
-        try(var connection = getConnection()) {
-            new MigrationStrategy(connection).executeMigration();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        mainMenu.execute();
     }
 }
